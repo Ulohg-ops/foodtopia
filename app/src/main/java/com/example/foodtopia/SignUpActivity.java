9816a7bdeaffa8673f76ebd7d0ec66c2be2c772d
Jacq <euv0104@gmail.com>
2022-01-27 14:54:52 +0800

    register

--- app/src/main/java/com/example/foodtopia/SignUpActivity.java
+++ app/src/main/java/com/example/foodtopia/SignUpActivity.java
@@ -1,14 +1,97 @@
 package com.example.foodtopia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //get Firebase auth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        //name the button
+        Button btn_LogIn = (Button) findViewById(R.id.button_login);
+        Button btn_SignUp = (Button) findViewById(R.id.button_signup);
+        EditText input_Email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        EditText input_Password = (EditText) findViewById(R.id.editTextTextPassword);
+        Button btnResetPassword = (Button) findViewById(R.id.button_reset);
+
+//        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(SignUpActivity.this, ResetPasswordActivity.class));
+//            }
+//        });
+
+        btn_LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btn_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = input_Email.getText().toString().trim();
+                String password = input_Password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6){
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                }
+
+                //not yet create progressBar
+                //progressBar.setVisibility(View.VISIBLE);
+
+                //create user
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUpActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                //progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed." + task.getException(), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
     }
+
+
 }
